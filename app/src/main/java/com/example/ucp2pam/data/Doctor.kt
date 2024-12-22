229f98ea489851ee40e9fa3a6ab7b7a03f3071fe
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-22 18:00:42 +0700

    Memperbarui halaman dokter dan jadwal menjadi satu file

---
+++ app/src/main/java/com/example/ucp2pam/data/Doctor.kt
@@ -0,0 +1,24 @@
+package com.example.ucp2pam.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dokter")
+data class Dokter(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val noHp: String,
+    val jamKerja: String
+)
+
+@Entity(tableName = "jadwal")
+data class Jadwal(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val namaDokter: String,
+    val namaPasien: String,
+    val noHp: String,
+    val tanggalKonsultasi: String,
+    val status: String
+)
