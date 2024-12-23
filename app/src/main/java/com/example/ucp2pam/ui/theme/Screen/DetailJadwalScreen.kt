b7fb08172c21b076c0f2e8e312f0c25d9029c721
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-23 12:13:40 +0700

    Menambahkan Background Color

--- app/src/main/java/com/example/ucp2pam/ui/theme/Screen/DetailJadwalScreen.kt
+++ app/src/main/java/com/example/ucp2pam/ui/theme/Screen/DetailJadwalScreen.kt
@@ -1,6 +1,7 @@
 package com.example.ucp2pam.ui.theme.Screen
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -28,6 +29,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -112,6 +114,7 @@ fun JadwalCard(jadwal: Jadwal, navController: NavController, viewModel: MainView
         Column(
             modifier = Modifier
                 .padding(16.dp)
+                .background(Color(0xFFACACAC))
         ) {
 
             Text(
