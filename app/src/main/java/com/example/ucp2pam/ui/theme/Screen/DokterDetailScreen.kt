7f5f39f1b2f453160553a8605564bde0dbe28b9f
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-23 14:19:14 +0700

    Menampilkan nama dokter

--- app/src/main/java/com/example/ucp2pam/ui/theme/Screen/DokterDetailScreen.kt
+++ app/src/main/java/com/example/ucp2pam/ui/theme/Screen/DokterDetailScreen.kt
@@ -149,3 +149,10 @@ fun DokterCard(dokter: Dokter, navController: NavController, onDelete: () -> Uni
                 modifier = Modifier
                     .padding(bottom = 8.dp)
             )
+            Text("Nama: ${dokter.nama}", style = MaterialTheme.typography.bodyMedium)
+            Text("Spesialis: ${dokter.spesialis}", style = MaterialTheme.typography.bodySmall,color = spesialisColor)
+            Text("Klinik: ${dokter.klinik}", style = MaterialTheme.typography.bodySmall)
+            Text("No HP: ${dokter.noHp}", style = MaterialTheme.typography.bodySmall)
+            Text("Jam Kerja: ${dokter.jamKerja}", style = MaterialTheme.typography.bodySmall)
+
+
