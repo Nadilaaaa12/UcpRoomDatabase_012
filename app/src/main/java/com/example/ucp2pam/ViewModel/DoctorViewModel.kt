ddaa090356bc7d2df55b377d87905d8834679f8f
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-24 13:02:41 +0700

    Untuk menambah jadwal

--- app/src/main/java/com/example/ucp2pam/ViewModel/DoctorViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ViewModel/DoctorViewModel.kt
@@ -31,3 +31,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
+
+    fun addJadwal(jadwal: Jadwal) {
+        viewModelScope.launch {
+            jadwalDao.insertJadwal(jadwal)
+        }
+    }
+
