ae0bccf8009d003c96f1685b5d59a5f1ea830548
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-12-24 12:59:32 +0700

    LiveData Dokter dan Jadwal

---
+++ app/src/main/java/com/example/ucp2pam/ViewModel/DoctorViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.ucp2pam.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.data.AppDatabase
+import com.example.ucp2pam.data.Dokter
+import com.example.ucp2pam.data.Jadwal
+import kotlinx.coroutines.launch
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val database = AppDatabase.getDatabase(application)
+    private val dokterDao = database.dokterDao()
+    private val jadwalDao = database.jadwalDao()
+
+
+    val dokterList: LiveData<List<Dokter>> = dokterDao.getAllDokter().asLiveData()
+    val jadwalList: LiveData<List<Jadwal>> = jadwalDao.getAllJadwal().asLiveData()
+
